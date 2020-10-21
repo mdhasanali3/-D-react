5022f986f70932811a962822eb7fe648a43b6bcf
mdhasanali3 <hasan48@student.sust.edu>
2020-10-22 00:49:57 +0600

    Updates

---
+++ precache-manifest.b6e13e7f13c2719142735d49553fd3fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504854d5164777038be7b832d97f8ec4",
+    "url": "/-d-react/index.html"
+  },
+  {
+    "revision": "c409aa202de24f25fa3d",
+    "url": "/-d-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1a1b8200fd90f9dbadc4",
+    "url": "/-d-react/static/css/main.aece7e4f.chunk.css"
+  },
+  {
+    "revision": "c409aa202de24f25fa3d",
+    "url": "/-d-react/static/js/2.6ced7727.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/-d-react/static/js/2.6ced7727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1b8200fd90f9dbadc4",
+    "url": "/-d-react/static/js/main.4fce2016.chunk.js"
+  },
+  {
+    "revision": "2d3dbff4c602dfa2ee2d",
+    "url": "/-d-react/static/js/runtime-main.1b4d41cb.js"
+  }
+]);
